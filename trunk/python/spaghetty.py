bf03615b90c2ee2e69682e9f5b3eea4e3f3ca5ff
jeff.science <jeff.science@65e94c06-8651-0410-b10c-61153dc42850>
2008-07-08 20:17:19 +0000

    Not much progress.
    git-svn-id: https://spaghetty.googlecode.com/svn@10 65e94c06-8651-0410-b10c-61153dc42850

--- trunk/python/spaghetty.py
+++ trunk/python/spaghetty.py
@@ -5,28 +5,48 @@ import os
 
 class Subroutine:
     
-   def __init__(self,language):
-      """Creates an empty subroutine class"""
-      self.language = language
-      self.name = []
-
+    def __init__(self,language):
+        """Creates a subroutine class"""
+        self.language = language
+        self.name = []
 
 
 class Language:
     
     def __init__(self,language):
-      if (self.language == "Fortran77"):
-         self.comment = "C     "
-         self.indent  = "      "
-      elif (self.language == "Fortran90"):
-         self.comment = "! "
-         self.indent  = ""
-      elif (self.language == "C"):
-         self.comment = "// "
-         self.indent  = ""
-      elif (self.language == "C++"):
-         self.comment = "// "
-         self.indent  = ""
-      else:
-         return "Unknown language"
-
+        """Creates a language class"""        
+        self.language = language
+        if (self.language == "Fortran77"):
+            self.comment   = "C     "
+            self.indent    = "      "
+        elif (self.language == "Fortran90"):
+            self.comment = "! "
+            self.indent  = ""
+        elif (self.language == "C"):
+            self.comment = "// "
+            self.indent  = ""
+        elif (self.language == "C++"):
+            self.comment = "// "
+            self.indent  = ""
+        elif (self.language == "Cuda"):
+            self.comment = "// "
+            self.indent  = ""
+        else:
+            return "Unsupported language"
+        
+        
+    def __str__(self):
+        """Prints code"""
+        print "Printing yet enabled."
+        
+        
+class Loop:        
+    
+    def __init__(self):
+        """Initializes a do/for loop"""
+        loop_offset = []
+        loop_unrolling = []
+        loop_index = []
+        index_begin = []
+        index_end = []
+        index_stride = []
