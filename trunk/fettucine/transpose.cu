47c923d16462f92aa8f835adb588e9c80986a7ee
jeff.science <jeff.science@65e94c06-8651-0410-b10c-61153dc42850>
2008-08-22 04:04:02 +0000

    Fixed copyrights a second time.  I am an idiot.
    git-svn-id: https://spaghetty.googlecode.com/svn@73 65e94c06-8651-0410-b10c-61153dc42850

--- trunk/fettucine/transpose.cu
+++ trunk/fettucine/transpose.cu
@@ -1,46 +1,5 @@
 /*
- * Copyright 1993-2007 NVIDIA Corporation.  All rights reserved.
- *
- * NOTICE TO USER:
- *
- * This source code is subject to NVIDIA ownership rights under U.S. and
- * international Copyright laws.  Users and possessors of this source code
- * are hereby granted a nonexclusive, royalty-free license to use this code
- * in individual and commercial software.
- *
- * NVIDIA MAKES NO REPRESENTATION ABOUT THE SUITABILITY OF THIS SOURCE
- * CODE FOR ANY PURPOSE.  IT IS PROVIDED "AS IS" WITHOUT EXPRESS OR
- * IMPLIED WARRANTY OF ANY KIND.  NVIDIA DISCLAIMS ALL WARRANTIES WITH
- * REGARD TO THIS SOURCE CODE, INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL NVIDIA BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL,
- * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
- * OF USE, DATA OR PROFITS,  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
- * OR OTHER TORTIOUS ACTION,  ARISING OUT OF OR IN CONNECTION WITH THE USE
- * OR PERFORMANCE OF THIS SOURCE CODE.
- *
- * U.S. Government End Users.   This source code is a "commercial item" as
- * that term is defined at  48 C.F.R. 2.numIterations1 (OCT 1995), consisting  of
- * "commercial computer  software"  and "commercial computer software
- * documentation" as such terms are  used in 48 C.F.R. 12.212 (SEPT 1995)
- * and is provided to the U.S. Government only as a commercial end item.
- * Consistent with 48 C.F.R.12.212 and 48 C.F.R. 227.7202-1 through
- * 227.7202-4 (JUNE 1995), all U.S. Government End Users acquire the
- * source code with only those rights set forth herein.
- *
- * Any use of this source code in individual and commercial software must
- * include, in the user documentation and internal comments to the code,
- * the above Disclaimer and U.S. Government End Users Notice.
- */
- 
-/* Matrix transpose with Cuda 
- * Host code.
-
- * This example transposes arbitrary-size matrices.  It compares a naive
- * transpose kernel that suffers from non-coalesced writes, to an optimized
- * transpose with fully coalesced memory access and no bank conflicts.  On 
- * a G80 GPU, the optimized transpose can be more than 10x faster for large
- * matrices.
+ * My heavily modified version of the NVIDIA transpose code.
  */
 
 // includes, system
@@ -129,11 +88,11 @@ runTest( int argc, char** argv)
                                 cudaMemcpyHostToDevice) );
 
     // setup execution parameters
-    dim3 grid(size_a / BLOCK_DIM, size_b / BLOCK_DIM, 1);
-    dim3 threads(BLOCK_DIM, BLOCK_DIM, 1);
+    dim3 dimGrid();
+    dim3 dimBlock();
 
     // warmup so we don't time CUDA startup
-    transpose<<< grid, threads >>>(d_odata, d_idata, size_a, size_b, size_c, size_d);
+    transpose<<< dimGrid, dimBlock >>>(d_odata, d_idata, size_a, size_b, size_c, size_d);
     
     int numIterations = 1;
 
