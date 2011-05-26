bd256bf9c100cd994f453aad7a92d11037418a91
jeff.science@gmail.com <jeff.science@gmail.com@65e94c06-8651-0410-b10c-61153dc42850>
2011-05-26 02:32:56 +0000

    Jeff: better idea
    git-svn-id: https://spaghetty.googlecode.com/svn@149 65e94c06-8651-0410-b10c-61153dc42850

---
+++ trunk/fettucine/util.h
@@ -0,0 +1,7 @@
+void init4d(int dim1, int dim2, int dim3, int dim4, double * ptr);
+void zero4d(int dim1, int dim2, int dim3, int dim4, double * ptr);
+void copy4d(int dim1, int dim2, int dim3, int dim4, double * in, double * out);
+void rand4d(int dim1, int dim2, int dim3, int dim4, double * ptr);
+double diff4d(int dim1, int dim2, int dim3, int dim4, double * a, double * b);
+void print4d(int dim1, int dim2, int dim3, int dim4, double * ptr);
+void print4d2(int dim1, int dim2, int dim3, int dim4, double * a, double * b);
