2a3db639911a997c406a3e45b38354cda67fcb29
jeff.science@gmail.com <jeff.science@gmail.com@65e94c06-8651-0410-b10c-61153dc42850>
2011-05-26 02:28:59 +0000

    all sorts of updates to generator and test code add Hirata NWChem code for correctness checking
    git-svn-id: https://spaghetty.googlecode.com/svn@148 65e94c06-8651-0410-b10c-61153dc42850

--- trunk/fettucine/generator.cpp
+++ trunk/fettucine/generator.cpp
@@ -41,9 +41,8 @@ privately owned rights.
 
 #include "generator.hpp"
 
-int main(int argc,char* argv[])
+void generateSource()
 {
-
     for (int a=1;a<=4;a++)
         for (int b=1;b<=4;b++)
             for (int c=1;c<=4;c++)
@@ -70,6 +69,95 @@ int main(int argc,char* argv[])
                                         }
                         fb.close();
                     }
+    return;
+}
+
+void generateTest(int dim1, int dim2, int dim3, int dim4)
+{
+    std::filebuf fb;
+    std::stringstream filename;
+    filename << "test.c";
+    fb.open (filename.str().c_str(),std::ios::out);
+    std::ostream os(&fb);
+
+    const int count = 100;
+
+    os << "#include <stdio.h>\n";
+    os << "#include <stdlib.h>\n";
+    os << "#include <math.h>\n";
+    //os << "#include <omp.h>\n";
+    os << "\n";
+    os << "unsigned long long getticks(void);\n";
+    os << "void init4d(int dim1, int dim2, int dim3, int dim4, double * ptr);\n";
+    os << "void zero4d(int dim1, int dim2, int dim3, int dim4, double * ptr);\n";
+    os << "void rand4d(int dim1, int dim2, int dim3, int dim4, double * ptr);\n";
+    os << "double diff4d(int dim1, int dim2, int dim3, int dim4, double * a, double * b);\n";
+    os << "void print4d(int dim1, int dim2, int dim3, int dim4, double * ptr);\n";
+    os << "void print4d2(int dim1, int dim2, int dim3, int dim4, double * a, double * b);\n";
+    os << "\n";
+    os << "int main()\n{\n";
+    //os << "    double t0, t1;\n";
+    os << "    unsigned long long t0, t1;\n";
+    os << "\n";
+    os << "    double * iptr = (double*) malloc(" << dim1 << "*" << dim2 << "*" << dim3 << "*" << dim4 << "*sizeof(double));\n";
+    os << "    double * optr = (double*) malloc(" << dim1 << "*" << dim2 << "*" << dim3 << "*" << dim4 << "*sizeof(double));\n";
+    os << "\n";
+    os << "    rand4d(" << dim1 << "," << dim2 << "," << dim3 << "," << dim4 << ",iptr);\n";
+    os << "    zero4d(" << dim1 << "," << dim2 << "," << dim3 << "," << dim4 << ",optr);\n";
+    os << "\n";
+
+    int a=4, b=3, c=2, d=1;
+    int i=2, j=3, k=4, l=1;
+
+    //    for (int a=1;a<=4;a++)
+//        for (int b=1;b<=4;b++)
+//            for (int c=1;c<=4;c++)
+//                for (int d=1;d<=4;d++)
+
+                    if (a!=b && a!=c && a!=d && b!=c && b!=d && c!=d)
+                    {
+                        os << "    printf(\"===========================================\\n\");\n";
+
+//                        for (int i=1;i<=4;i++)
+//                            for (int j=1;j<=4;j++)
+//                                for (int k=1;k<=4;k++)
+//                                    for (int l=1;l<=4;l++)
+
+                                        if (i!=j && i!=k && i!=l && j!=k && j!=l && k!=l)
+                                        {
+                                            //os << "    t0 = omp_get_wtime();\n";
+                                            os << "    t0 = getticks();\n";
+                                            std::stringstream function;
+                                            function << "permute_" << a << b << c << d << "_"<< i << j << k << l;
+                                            std::string fname = function.str();
+                                            os << "    for (int t=0;t<" << count << ";t++)" << fname << "(";
+                                            os << dim1 << "," << dim2 << "," << dim3 << "," << dim4 << ",";
+                                            os << "iptr" << "," << "optr";
+                                            os << ");\n";
+                                            //os << "    t1 = omp_get_wtime();\n";
+                                            os << "    t1 = getticks();\n";
+                                            //os << "    printf(\"" << fname << " took %lf seconds\\n\",(t1-t0)/" << count << ");\n";
+                                            os << "    printf(\"" << fname << " took %12llu cycles\\n\",(t1-t0)/" << count << ");\n";
+                                        }
+                    }
+
+    os << "    printf(\"===========================================\\n\");\n";
+    os << "\n";
+    os << "    print4d2(" << dim1 << "," << dim2 << "," << dim3 << "," << dim4 << ",iptr,optr);\n";
+    os << "\n";
+    os << "    free(iptr);\n";
+    os << "    free(optr);\n";
+    os << "\n";
+    os << "    return 0;\n}\n";
+    fb.close();
+
+    return;
+}
+
+int main(int argc,char* argv[])
+{
+    generateSource();
+    generateTest(3,3,3,3);
 
     return 0;
 }
--- trunk/fettucine/util.c
+++ trunk/fettucine/util.c
@@ -1,9 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void zero4d(int dim1, int dim2, int dim3, int dim4, double * ptr)
+{
+    for (int i=0;i<(dim1*dim2*dim3*dim4);i++) ptr[i] = 0.0;
+    return;
+}
+
+void rand4d(int dim1, int dim2, int dim3, int dim4, double * ptr)
+{
+    for (int i=0;i<(dim1*dim2*dim3*dim4);i++) ptr[i] = (double) rand()/RAND_MAX;
+    return;
+}
+
 void init4d(int dim1, int dim2, int dim3, int dim4, double * ptr)
 {
-    int shift1, shift2, shift3;
+    double shift1, shift2, shift3;
 
-    if (dim1<100 && dim2<100 && dim3<100 && dim4<100) shift1 = 100;
-    else shift1 = 1000;
+    if (dim1<100 && dim2<100 && dim3<100 && dim4<100) shift1 = 0.001;
+    else shift1 = 0.0001;
 
     shift2 = shift1*shift1;
     shift3 = shift2*shift1;
@@ -12,14 +28,50 @@ void init4d(int dim1, int dim2, int dim3, int dim4, double * ptr)
       for (int j=0;j<dim2;j++)
         for (int k=0;k<dim3;k++)
           for (int l=0;l<dim4;l++)
-             ptr[i*dim2*dim3*dim4 + j*dim3*dim4 + k*dim4 + l] = (double) (i*shift3 + j*shift2 + k*shift1 + l);
+             ptr[i*dim2*dim3*dim4 + j*dim3*dim4 + k*dim4 + l] = (double) (i + j*shift1 + k*shift2 + l*shift3);
 
     return;
 }
 
-void zero4d(int dim1, int dim2, int dim3, int dim4, double * ptr)
+double diff4d(int dim1, int dim2, int dim3, int dim4, double * a, double * b)
 {
-    for (int i=0;i<(dim1*dim2*dim3*dim4);i++) ptr[i] = 0.0;
+    double error = 0.0;
+
+    for (int i=0;i<dim1;i++)
+      for (int j=0;j<dim2;j++)
+        for (int k=0;k<dim3;k++)
+          for (int l=0;l<dim4;l++)
+	  {
+             error += fabs( a[i*dim2*dim3*dim4 + j*dim3*dim4 + k*dim4 + l] - b[i*dim2*dim3*dim4 + j*dim3*dim4 + k*dim4 + l]);
+	  }
+
+    return error;
+}
+
+void print4d(int dim1, int dim2, int dim3, int dim4, double * ptr)
+{
+    for (int i=0;i<dim1;i++)
+      for (int j=0;j<dim2;j++)
+        for (int k=0;k<dim3;k++)
+          for (int l=0;l<dim4;l++)
+             printf("[%2d,%2d,%2d,%2d] = %20.14lf \n",i,j,k,l,ptr[i*dim2*dim3*dim4 + j*dim3*dim4 + k*dim4 + l]);
+
+    return;
+}
+
+void print4d2(int dim1, int dim2, int dim3, int dim4, double * a, double * b)
+{
+    for (int i=0;i<dim1;i++)
+      for (int j=0;j<dim2;j++)
+        for (int k=0;k<dim3;k++)
+          for (int l=0;l<dim4;l++)
+	  {
+             printf("[%2d,%2d,%2d,%2d] = ",i,j,k,l);
+             printf("%20.14lf ",a[i*dim2*dim3*dim4 + j*dim3*dim4 + k*dim4 + l]);
+             printf("%20.14lf ",b[i*dim2*dim3*dim4 + j*dim3*dim4 + k*dim4 + l]);
+             printf("\n");
+	  }
+
     return;
 }
 
