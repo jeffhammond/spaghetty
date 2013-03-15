0cf8524a33cccacc909911fe34efbcf5d882346c
jeff.science@gmail.com <jeff.science@gmail.com@65e94c06-8651-0410-b10c-61153dc42850>
2013-03-15 21:32:27 +0000

    pretty clean impl
    git-svn-id: https://spaghetty.googlecode.com/svn@178 65e94c06-8651-0410-b10c-61153dc42850

--- trunk/python/archive/final/generate_source_final.py
+++ trunk/python/archive/final/generate_source_final.py
@@ -13,50 +13,81 @@ def perm(l):
 
 indices = ['4','3','2','1']
 
-all_permutations = perm(indices)
-transpose_list = perm(indices)
-loop_list = perm(indices)
+Debug=True
+#Debug=False
 
-for OpenMP in [True,False]:
-    if OpenMP:
-        omp_name = 'omp'
-    else:
-        omp_name = 'nomp'
+if Debug:
+    all_permutations = [indices]
+    transpose_list   = [indices]
+    loop_list        = [indices]
+else:
+    all_permutations = perm(indices)
+    transpose_list   = perm(indices)
+    loop_list        = perm(indices)
 
-    for Factor in [1.0,-1.0,0.5,-0.5]:
-        if (Factor==1.0):
-            variant = omp_name+'_copy'    
-        elif (Factor==-1.0):
-            variant = omp_name+'_neg'    
-        elif (Factor==0.5):
-            variant = omp_name+'_half'    
-        elif (Factor==-0.5):
-            variant = omp_name+'_nhalf'    
+makefile = open('Makefile','w')
+makefile.write('FC      = gfortran \n')
+makefile.write('FFLAGS  = -O3 -fopenmp  \n\n')
+makefile.write('OBJECTS = \\\n')
 
-        for transpose_order in transpose_list:
-            A = transpose_order[0]
-            B = transpose_order[1]
-            C = transpose_order[2]
-            D = transpose_order[3]
-            for loop_order in loop_list:
-                a = loop_order[0]
-                b = loop_order[1]
-                c = loop_order[2]
-                d = loop_order[3]
-                subroutine_name = 'trans_'+A+B+C+D+'_loop_'+a+b+c+d+'_'+variant
-                source_name = subroutine_name+'.F'
-                source_file = open(source_name,'w')
+for transpose_order in transpose_list:
+    A = transpose_order[0]
+    B = transpose_order[1]
+    C = transpose_order[2]
+    D = transpose_order[3]
+    for loop_order in loop_list:
+        a = loop_order[0]
+        b = loop_order[1]
+        c = loop_order[2]
+        d = loop_order[3]
+
+        source_name = 'trans_'+A+B+C+D+'_loop_'+a+b+c+d
+        makefile.write(source_name+'.o \\\n')
+        source_file = open(source_name+'.f','w')
+
+        for OpenMP in [True,False]:
+            if OpenMP:
+                omp_name = 'omp'
+                omp_text = 'with OpenMP'
+            else:
+                omp_name = 'nomp'
+                omp_text = 'without OpenMP'
+
+            for Factor in [1.0,-1.0,37.0]:
+                if (Factor==1.0):
+                    fac_name = 'copy'
+                    fac_text = 'scale by 1.0'
+                elif (Factor==-1.0):
+                    fac_name = 'neg'
+                    fac_text = 'scale by -1.0'
+                else:
+                    fac_name = 'any'
+                    fac_text = 'scale by any value'
+
+                variant = omp_name+'_'+fac_name
+                subroutine_name = source_name+'_'+variant
+                print subroutine_name
+
+                source_file.write('! '+omp_text+', '+fac_text+'\n')
                 source_file.write('        subroutine '+subroutine_name+'(unsorted,sorted,\n')
-                source_file.write('     &                dim1,dim2,dim3,dim4)\n')
+                if Factor == 37.0:
+                    source_file.write('     &                dim1,dim2,dim3,dim4,factor)\n')
+                else:
+                    source_file.write('     &                dim1,dim2,dim3,dim4)\n')
                 source_file.write('        implicit none\n')
                 source_file.write('        integer dim1,dim2,dim3,dim4\n')
                 source_file.write('        integer j1,j2,j3,j4\n')
                 source_file.write('        double precision sorted(dim1*dim2*dim3*dim4)\n')
                 source_file.write('        double precision unsorted(dim1*dim2*dim3*dim4)\n')
+                if Factor == 37.0:
+                    source_file.write('        double precision factor\n')
                 if OpenMP:
                     source_file.write('!$omp parallel do collapse(4)\n')
                     source_file.write('!$omp& private(j1,j2,j3,j4)\n')
-                    source_file.write('!$omp& firstprivate(factor,dim1,dim2,dim3,dim4)\n')
+                    if Factor == 37.0:
+                        source_file.write('!$omp& firstprivate(dim1,dim2,dim3,dim4,factor)\n')
+                    else:
+                        source_file.write('!$omp& firstprivate(dim1,dim2,dim3,dim4)\n')
                     source_file.write('!$omp& shared(sorted,unsorted)\n')
                     source_file.write('!$omp& schedule(static)\n')
                 source_file.write('        do j'+a+' = 1,dim'+a+'\n')
@@ -69,12 +100,10 @@ for OpenMP in [True,False]:
                 elif (Factor==-1.0):
                     source_file.write('            sorted(j'+D+'+dim'+D+'*(j'+C+'-1+dim'+C+'*(j'+B+'-1+dim'+B+'*(j'+A+'-1)))) = \n')
                     source_file.write('     &    -unsorted(j4+dim4*(j3-1+dim3*(j2-1+dim2*(j1-1))))\n')
-                elif (Factor==0.5):
+                elif Factor == 37.0:
                     source_file.write('            sorted(j'+D+'+dim'+D+'*(j'+C+'-1+dim'+C+'*(j'+B+'-1+dim'+B+'*(j'+A+'-1)))) = \n')
-                    source_file.write('     &    0.5*unsorted(j4+dim4*(j3-1+dim3*(j2-1+dim2*(j1-1))))\n')
-                elif (Factor==-0.5):
-                    source_file.write('            sorted(j'+D+'+dim'+D+'*(j'+C+'-1+dim'+C+'*(j'+B+'-1+dim'+B+'*(j'+A+'-1)))) = \n')
-                    source_file.write('     &    -0.5*unsorted(j4+dim4*(j3-1+dim3*(j2-1+dim2*(j1-1))))\n')
+                    source_file.write('     &    factor*unsorted(j4+dim4*(j3-1+dim3*(j2-1+dim2*(j1-1))))\n')
+
                 source_file.write('           enddo\n')
                 source_file.write('          enddo\n')
                 source_file.write('         enddo\n')
@@ -82,6 +111,19 @@ for OpenMP in [True,False]:
                 if OpenMP:
                     source_file.write('!$omp end parallel do\n')
                 source_file.write('        return\n')
-                source_file.write('        end\n')
-                source_file.close()
+                source_file.write('        end\n\n')
 
+        source_file.close()
+
+makefile.write('\n\n')
+makefile.write('all: libspaghetty.a \n\n')
+makefile.write('libspaghetty.a: $(OBJECTS) \n')
+makefile.write('\t$(AR) $(ARFLAGS) $@ $< \n\n')
+makefile.write('%.o: %.f \n')
+makefile.write('\t$(FC) $(FFLAGS) -c $< -o $@ \n\n')
+makefile.write('clean: \n')
+makefile.write('\t$(RM) $(RMFLAGS) $(OBJECTS) \n\n')
+makefile.write('realclean: clean \n')
+makefile.write('\t$(RM) $(RMFLAGS) libspaghetty.a \n\n')
+
+makefile.close()
