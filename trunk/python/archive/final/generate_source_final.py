d9085d603b8cf3de618c0df53683c650c5f12833
jeff.science@gmail.com <jeff.science@gmail.com@65e94c06-8651-0410-b10c-61153dc42850>
2013-03-15 21:01:38 +0000

    move final
    git-svn-id: https://spaghetty.googlecode.com/svn@177 65e94c06-8651-0410-b10c-61153dc42850

---
+++ trunk/python/archive/final/generate_source_final.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+
+import fileinput
+import string
+import sys
+import os
+
+def perm(l):
+    sz = len(l)
+    if sz <= 1:
+        return [l]
+    return [p[:i]+[l[0]]+p[i:] for i in xrange(sz) for p in perm(l[1:])]
+
+indices = ['4','3','2','1']
+
+all_permutations = perm(indices)
+transpose_list = perm(indices)
+loop_list = perm(indices)
+
+for OpenMP in [True,False]:
+    if OpenMP:
+        omp_name = 'omp'
+    else:
+        omp_name = 'nomp'
+
+    for Factor in [1.0,-1.0,0.5,-0.5]:
+        if (Factor==1.0):
+            variant = omp_name+'_copy'    
+        elif (Factor==-1.0):
+            variant = omp_name+'_neg'    
+        elif (Factor==0.5):
+            variant = omp_name+'_half'    
+        elif (Factor==-0.5):
+            variant = omp_name+'_nhalf'    
+
+        for transpose_order in transpose_list:
+            A = transpose_order[0]
+            B = transpose_order[1]
+            C = transpose_order[2]
+            D = transpose_order[3]
+            for loop_order in loop_list:
+                a = loop_order[0]
+                b = loop_order[1]
+                c = loop_order[2]
+                d = loop_order[3]
+                subroutine_name = 'trans_'+A+B+C+D+'_loop_'+a+b+c+d+'_'+variant
+                source_name = subroutine_name+'.F'
+                source_file = open(source_name,'w')
+                source_file.write('        subroutine '+subroutine_name+'(unsorted,sorted,\n')
+                source_file.write('     &                dim1,dim2,dim3,dim4)\n')
+                source_file.write('        implicit none\n')
+                source_file.write('        integer dim1,dim2,dim3,dim4\n')
+                source_file.write('        integer j1,j2,j3,j4\n')
+                source_file.write('        double precision sorted(dim1*dim2*dim3*dim4)\n')
+                source_file.write('        double precision unsorted(dim1*dim2*dim3*dim4)\n')
+                if OpenMP:
+                    source_file.write('!$omp parallel do collapse(4)\n')
+                    source_file.write('!$omp& private(j1,j2,j3,j4)\n')
+                    source_file.write('!$omp& firstprivate(factor,dim1,dim2,dim3,dim4)\n')
+                    source_file.write('!$omp& shared(sorted,unsorted)\n')
+                    source_file.write('!$omp& schedule(static)\n')
+                source_file.write('        do j'+a+' = 1,dim'+a+'\n')
+                source_file.write('         do j'+b+' = 1,dim'+b+'\n')
+                source_file.write('          do j'+c+' = 1,dim'+c+'\n')
+                source_file.write('           do j'+d+' = 1,dim'+d+'\n')
+                if (Factor==1.0):
+                    source_file.write('            sorted(j'+D+'+dim'+D+'*(j'+C+'-1+dim'+C+'*(j'+B+'-1+dim'+B+'*(j'+A+'-1)))) = \n')
+                    source_file.write('     &    unsorted(j4+dim4*(j3-1+dim3*(j2-1+dim2*(j1-1))))\n')
+                elif (Factor==-1.0):
+                    source_file.write('            sorted(j'+D+'+dim'+D+'*(j'+C+'-1+dim'+C+'*(j'+B+'-1+dim'+B+'*(j'+A+'-1)))) = \n')
+                    source_file.write('     &    -unsorted(j4+dim4*(j3-1+dim3*(j2-1+dim2*(j1-1))))\n')
+                elif (Factor==0.5):
+                    source_file.write('            sorted(j'+D+'+dim'+D+'*(j'+C+'-1+dim'+C+'*(j'+B+'-1+dim'+B+'*(j'+A+'-1)))) = \n')
+                    source_file.write('     &    0.5*unsorted(j4+dim4*(j3-1+dim3*(j2-1+dim2*(j1-1))))\n')
+                elif (Factor==-0.5):
+                    source_file.write('            sorted(j'+D+'+dim'+D+'*(j'+C+'-1+dim'+C+'*(j'+B+'-1+dim'+B+'*(j'+A+'-1)))) = \n')
+                    source_file.write('     &    -0.5*unsorted(j4+dim4*(j3-1+dim3*(j2-1+dim2*(j1-1))))\n')
+                source_file.write('           enddo\n')
+                source_file.write('          enddo\n')
+                source_file.write('         enddo\n')
+                source_file.write('        enddo\n')
+                if OpenMP:
+                    source_file.write('!$omp end parallel do\n')
+                source_file.write('        return\n')
+                source_file.write('        end\n')
+                source_file.close()
+
