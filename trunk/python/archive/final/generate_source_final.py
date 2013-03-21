a905277af6a19312eee44302f4bb09dbd5361921
jeff.science@gmail.com <jeff.science@gmail.com@65e94c06-8651-0410-b10c-61153dc42850>
2013-03-21 15:32:26 +0000

    subroutine generation done; time to integrate tester
    git-svn-id: https://spaghetty.googlecode.com/svn@184 65e94c06-8651-0410-b10c-61153dc42850

--- trunk/python/archive/final/generate_source_final.py
+++ trunk/python/archive/final/generate_source_final.py
@@ -5,6 +5,10 @@ import string
 import sys
 import os
 
+#Debug=True
+Debug=False
+
+
 def perm(l):
     sz = len(l)
     if sz <= 1:
@@ -61,94 +65,109 @@ def generate_subroutine(ofile, name, description, OpenMP, Factor, transpose_orde
     b = loop_order[1]
     c = loop_order[2]
     d = loop_order[3]
-    source_file.write(description)
-    source_file.write('        subroutine '+name+'(unsorted,sorted,\n')
+    ofile.write(description)
+    ofile.write('        subroutine '+name+'(unsorted,sorted,\n')
     if Factor not in [1.0,-1.0]:
-        source_file.write('     &                dim1,dim2,dim3,dim4,factor)\n')
+        ofile.write('     &                dim1,dim2,dim3,dim4,factor)\n')
     else:
-        source_file.write('     &                dim1,dim2,dim3,dim4)\n')
-    source_file.write('        implicit none\n')
-    source_file.write('        integer dim1,dim2,dim3,dim4\n')
-    source_file.write('        integer j1,j2,j3,j4\n')
-    source_file.write('        double precision sorted(dim1*dim2*dim3*dim4)\n')
-    source_file.write('        double precision unsorted(dim1*dim2*dim3*dim4)\n')
+        ofile.write('     &                dim1,dim2,dim3,dim4)\n')
+    ofile.write('        implicit none\n')
+    ofile.write('        integer dim1,dim2,dim3,dim4\n')
+    ofile.write('        integer j1,j2,j3,j4\n')
+    ofile.write('        double precision sorted(dim1*dim2*dim3*dim4)\n')
+    ofile.write('        double precision unsorted(dim1*dim2*dim3*dim4)\n')
     if Factor not in [1.0,-1.0]:
-        source_file.write('        double precision factor\n')
+        ofile.write('        double precision factor\n')
     if OpenMP:
-        source_file.write('!$omp parallel do collapse(4)\n')
-        source_file.write('!$omp& private(j1,j2,j3,j4)\n')
+        ofile.write('!$omp parallel do collapse(4)\n')
+        ofile.write('!$omp& private(j1,j2,j3,j4)\n')
         if Factor not in [1.0,-1.0]:
-            source_file.write('!$omp& firstprivate(dim1,dim2,dim3,dim4,factor)\n')
+            ofile.write('!$omp& firstprivate(dim1,dim2,dim3,dim4,factor)\n')
         else:
-            source_file.write('!$omp& firstprivate(dim1,dim2,dim3,dim4)\n')
-        source_file.write('!$omp& shared(sorted,unsorted)\n')
-        source_file.write('!$omp& schedule(static)\n')
-    source_file.write('        do j'+a+' = 1,dim'+a+'\n')
-    source_file.write('         do j'+b+' = 1,dim'+b+'\n')
-    source_file.write('          do j'+c+' = 1,dim'+c+'\n')
-    source_file.write('           do j'+d+' = 1,dim'+d+'\n')
+            ofile.write('!$omp& firstprivate(dim1,dim2,dim3,dim4)\n')
+        ofile.write('!$omp& shared(sorted,unsorted)\n')
+        ofile.write('!$omp& schedule(static)\n')
+    ofile.write('        do j'+a+' = 1,dim'+a+'\n')
+    ofile.write('         do j'+b+' = 1,dim'+b+'\n')
+    ofile.write('          do j'+c+' = 1,dim'+c+'\n')
+    ofile.write('           do j'+d+' = 1,dim'+d+'\n')
     if (Factor==1.0):
-        source_file.write('            sorted(j'+D+'+dim'+D+'*(j'+C+'-1+dim'+C+'*(j'+B+'-1+dim'+B+'*(j'+A+'-1)))) = \n')
-        source_file.write('     &    unsorted(j4+dim4*(j3-1+dim3*(j2-1+dim2*(j1-1))))\n')
+        ofile.write('            sorted(j'+D+'+dim'+D+'*(j'+C+'-1+dim'+C+'*(j'+B+'-1+dim'+B+'*(j'+A+'-1)))) = \n')
+        ofile.write('     &    unsorted(j4+dim4*(j3-1+dim3*(j2-1+dim2*(j1-1))))\n')
     elif (Factor==-1.0):
-        source_file.write('            sorted(j'+D+'+dim'+D+'*(j'+C+'-1+dim'+C+'*(j'+B+'-1+dim'+B+'*(j'+A+'-1)))) = \n')
-        source_file.write('     &    -unsorted(j4+dim4*(j3-1+dim3*(j2-1+dim2*(j1-1))))\n')
+        ofile.write('            sorted(j'+D+'+dim'+D+'*(j'+C+'-1+dim'+C+'*(j'+B+'-1+dim'+B+'*(j'+A+'-1)))) = \n')
+        ofile.write('     &    -unsorted(j4+dim4*(j3-1+dim3*(j2-1+dim2*(j1-1))))\n')
     else:
-        source_file.write('            sorted(j'+D+'+dim'+D+'*(j'+C+'-1+dim'+C+'*(j'+B+'-1+dim'+B+'*(j'+A+'-1)))) = \n')
-        source_file.write('     &    factor*unsorted(j4+dim4*(j3-1+dim3*(j2-1+dim2*(j1-1))))\n')
+        ofile.write('            sorted(j'+D+'+dim'+D+'*(j'+C+'-1+dim'+C+'*(j'+B+'-1+dim'+B+'*(j'+A+'-1)))) = \n')
+        ofile.write('     &    factor*unsorted(j4+dim4*(j3-1+dim3*(j2-1+dim2*(j1-1))))\n')
 
-    source_file.write('           enddo\n')
-    source_file.write('          enddo\n')
-    source_file.write('         enddo\n')
-    source_file.write('        enddo\n')
+    ofile.write('           enddo\n')
+    ofile.write('          enddo\n')
+    ofile.write('         enddo\n')
+    ofile.write('        enddo\n')
     if OpenMP:
-        source_file.write('!$omp end parallel do\n')
-    source_file.write('        return\n')
-    source_file.write('        end\n\n')
+        ofile.write('!$omp end parallel do\n')
+    ofile.write('        return\n')
+    ofile.write('        end\n\n')
     return
 
 
-#Debug=True
-Debug=False
+def generate_all_subroutines(Debug):
+    for transpose_order in generate_permutation_list(Debug):
+        for loop_order in generate_permutation_list(Debug):
+
+            source_name = 'trans_'+perm_to_string(transpose_order)+'_loop_'+perm_to_string(loop_order)
+            source_file = open(source_name+'.f','w')
+
+            for OpenMP in [True,False]:
+                (omp_name,omp_text) = get_omp_info(OpenMP)
+
+                for Factor in [1.0,-1.0,37.0]:
+                    (fac_name,fac_text) = get_fac_info(Factor)
+
+                    variant = omp_name+'_'+fac_name
+                    subroutine_name = source_name+'_'+variant
+                    print subroutine_name
+                    description = '! '+omp_text+', '+fac_text+'\n'
+
+                    generate_subroutine(source_file, subroutine_name, description, OpenMP, Factor, transpose_order, loop_order)
+
+            source_file.close()
 
 
-makefile = open('Makefile','w')
-makefile.write('FC      = gfortran \n')
-makefile.write('FFLAGS  = -O3 -fopenmp  \n\n')
-makefile.write('OBJECTS = \\\n')
+def generate_makefile(Debug):
+    makefile = open('Makefile','w')
+    makefile.write('FC      = gfortran \n')
+    if (Debug):
+        makefile.write('FFLAGS  = -g -O0 -Wall -fopenmp  \n\n')
+    else:
+        makefile.write('FFLAGS  = -O3 -fopenmp  \n\n')
 
-for transpose_order in generate_permutation_list(Debug):
-    for loop_order in generate_permutation_list(Debug):
+    makefile.write('SOURCES = \\\n')
+    for transpose_order in generate_permutation_list(Debug):
+        for loop_order in generate_permutation_list(Debug):
+            source_name = 'trans_'+perm_to_string(transpose_order)+'_loop_'+perm_to_string(loop_order)
+            makefile.write(source_name+'.f \\\n')
 
-        source_name = 'trans_'+perm_to_string(transpose_order)+'_loop_'+perm_to_string(loop_order)
-        makefile.write(source_name+'.o \\\n')
-        source_file = open(source_name+'.f','w')
+    makefile.write('OBJECTS = \\\n')
+    for transpose_order in generate_permutation_list(Debug):
+        for loop_order in generate_permutation_list(Debug):
+            source_name = 'trans_'+perm_to_string(transpose_order)+'_loop_'+perm_to_string(loop_order)
+            makefile.write(source_name+'.o \\\n')
 
-        for OpenMP in [True,False]:
-            (omp_name,omp_text) = get_omp_info(OpenMP)
-
-            for Factor in [1.0,-1.0,37.0]:
-                (fac_name,fac_text) = get_fac_info(Factor)
-
-                variant = omp_name+'_'+fac_name
-                subroutine_name = source_name+'_'+variant
-                print subroutine_name
-                description = '! '+omp_text+', '+fac_text+'\n'
-
-                generate_subroutine(source_file, subroutine_name, description, OpenMP, Factor, transpose_order, loop_order)
-
-        source_file.close()
+    makefile.write('\n\n')
+    makefile.write('all: libspaghetty.a \n\n')
+    makefile.write('libspaghetty.a: $(OBJECTS) \n')
+    makefile.write('\t$(AR) $(ARFLAGS) $@ $(OBJECTS) \n\n')
+    makefile.write('%.o: %.f \n')
+    makefile.write('\t$(FC) $(FFLAGS) -c $< -o $@ \n\n')
+    makefile.write('clean: \n')
+    makefile.write('\t$(RM) $(RMFLAGS) $(OBJECTS) \n\n')
+    makefile.write('realclean: clean \n')
+    makefile.write('\t$(RM) $(RMFLAGS) libspaghetty.a $(SOURCES) \n\n')
+    makefile.close()
+    return
 
 
-makefile.write('\n\n')
-makefile.write('all: libspaghetty.a \n\n')
-makefile.write('libspaghetty.a: $(OBJECTS) \n')
-makefile.write('\t$(AR) $(ARFLAGS) $@ $(OBJECTS) \n\n')
-makefile.write('%.o: %.f \n')
-makefile.write('\t$(FC) $(FFLAGS) -c $< -o $@ \n\n')
-makefile.write('clean: \n')
-makefile.write('\t$(RM) $(RMFLAGS) $(OBJECTS) \n\n')
-makefile.write('realclean: clean \n')
-makefile.write('\t$(RM) $(RMFLAGS) libspaghetty.a *.f \n\n')
-
-makefile.close()
+generate_all_subroutines(Debug)
+generate_makefile(Debug)
