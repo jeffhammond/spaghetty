ac1ef451f8db399d2f0959d5b458357535a1013a
jeff.science@gmail.com <jeff.science@gmail.com@65e94c06-8651-0410-b10c-61153dc42850>
2013-03-21 17:55:48 +0000

    rename so i can have both f and c utils
    git-svn-id: https://spaghetty.googlecode.com/svn@188 65e94c06-8651-0410-b10c-61153dc42850

--- trunk/python/archive/final/generate_tester_final.py
+++ trunk/python/archive/final/generate_tester_final.py
@@ -5,83 +5,6 @@ import string
 import sys
 import os
 
-mpi = True
-if ( mpi ):
-    print 'Using MPI'
-else:
-    print 'Not using MPI'
-
-#OpenMP = True
-OpenMP = False
-if ( OpenMP ):
-    print 'Using OpenMP'
-else:
-    print 'Not using OpenMP'
-
-fortran_compiler = 'ifort'
-if (OpenMP):
-        fortran_opt_flags = '-g -i8 -O3 -openmp -xSSE4.2 -opt-prefetch -funroll-loops'
-else:
-        fortran_opt_flags = '-g -i8 -O3 -xSSE4.2 -opt-prefetch -funroll-loops'
-
-if ( mpi ):
-    fortran_linker = 'mpif77'
-else:
-    fortran_linker = 'ifort'
-
-fortran_link_flags = fortran_opt_flags
-
-src_dir = '~/spaghetty/python/archive/src/'
-exe_dir = '~/spaghetty/python/archive/exe/'
-
-if (OpenMP):
-        lib_name = 'tce_sort_intel_omp.a'
-else:
-        lib_name = 'tce_sort_intel.a'
-
-flush_rank='1000'
-
-count = '100'
-rank  = '40'
-ranks = [rank,rank,rank,rank]
-size  =  int(ranks[0])*int(ranks[1])*int(ranks[2])*int(ranks[3])
-sizechar = str(size)
-
-def perm(l):
-    sz = len(l)
-    if sz <= 1:
-        return [l]
-    return [p[:i]+[l[0]]+p[i:] for i in xrange(sz) for p in perm(l[1:])]
-
-indices = ['4','3','2','1']
-
-#transpose_list = [['1','3','2','4']]
-transpose_list = perm(indices)
-loop_list = perm(indices)
-
-print fortran_compiler+' '+fortran_opt_flags+' -c tce_sort_hirata.F'
-os.system(fortran_compiler+' '+fortran_opt_flags+' -c tce_sort_hirata.F')
-os.system('ar -r '+lib_name+' tce_sort_hirata.o')
-
-#timer = 'jeff'
-#timer_call = "jefftime()"
-#print fortran_compiler+' '+fortran_opt_flags+' -c jefftime.F'
-#os.system(fortran_compiler+' '+fortran_opt_flags+' -c jefftime.F')
-#os.system('ar -r '+lib_name+' jefftime.o')
-
-if ( mpi ):
-    timer = 'mpi'
-    timer_call = "mpi_wtime()"
-
-#timer = 'ticks'
-#timer_call = "getticks()"
-
-#timer = 'time'
-#timer_call = "gettime()"
-#print 'icc -c getticks_linux.c'
-#os.system('icc -c getticks_linux.c')
-#os.system('ar -r '+lib_name+' getticks_linux.o')
-
 for transpose_order in transpose_list:
     dummy = 0
     A = transpose_order[0]
@@ -343,9 +266,6 @@ for transpose_order in transpose_list:
     source_file.write(' 1100 format(1x,a16,4f12.6)\n')
     source_file.write('      END\n')
     source_file.close()
-    print fortran_linker+' '+fortran_link_flags+' '+' '+source_name+' '+lib_name+' '+' -o '+exe_dir+driver_name+'.x'
-    os.system(fortran_linker+' '+fortran_link_flags+' '+' '+source_name+' '+lib_name+' '+' -o '+exe_dir+driver_name+'.x')
-    os.system('mv '+source_name+' '+src_dir)
 
 
 
