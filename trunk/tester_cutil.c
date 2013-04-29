9872858dc2fd31998b9d52b7ea730f2b8836d7de
jeff.science@gmail.com <jeff.science@gmail.com@65e94c06-8651-0410-b10c-61153dc42850>
2013-04-29 14:58:04 +0000

    add f_memcpy and symmetrize calling of impl
    git-svn-id: https://spaghetty.googlecode.com/svn@243 65e94c06-8651-0410-b10c-61153dc42850

--- trunk/tester_cutil.c
+++ trunk/tester_cutil.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
 #ifdef _OPENMP
 #include <omp.h>
@@ -10,7 +11,7 @@
 #include <mpi.h>
 #endif
 
-double wtime(void)
+static inline double wtime_impl(void)
 {
     double t = 0.0;
 #if   defined(_OPENMP)
@@ -25,12 +26,43 @@ double wtime(void)
     return t;
 }
 
+double wtime(void)
+{
+    return wtime_impl();
+}
+
 double wtime_(void)
 {
-    return wtime();
+    return wtime_impl();
 }
 
 double wtime__(void)
 {
-    return wtime();
+    return wtime_imple();
 }
+
+/* memcpy(void *restrict s1, const void *restrict s2, size_t n); */
+
+/* Fortran does not respect const but requires no aliasing... */
+static inline void f_memcpy_impl(void * restrict new, void * restrict old, int * length)
+{
+  const size_t l = (size_t) *length;
+  memcpy(new,old,l);
+  return;
+}
+
+void f_memcpy(void * restrict new, void * restrict old, int * length)
+{
+  return f_memcpy_impl(new,old,length);
+}
+
+void f_memcpy_(void * restrict new, void * restrict old, int * length)
+{
+  return f_memcpy_impl(new,old,length);
+}
+
+void f_memcpy__(void * restrict new, void * restrict old, int * length)
+{
+  return f_memcpy_impl(new,old,length);
+}
+
